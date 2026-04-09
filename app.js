7d22633e64782cf95b699abaa0eea3581e30fc68
viveksingh007e <viveksingh007e@users.noreply.github.com>
2026-04-09 18:23:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,12 +38,11 @@ function startEyeExercise() {
 function startBellyExercise() {
     runExercise([
         ['Welcome dear! We will start belly exercise now. This will last just 8 minutes. Take your time to get in position. We will start in 10 seconds', 20],
-        ['Are you ready now!', 5],
-        ['Lets start standing knee raises for 2 minutes. Starting now!', 125],
+        ['Lets start standing knee raises for 1.5 minutes. Starting now!', 95],
         ['Great! Relax now for 10 seconds', 12],
-        ['Lets start standing side bends for 2 minutes. Starting now!', 125],
+        ['Lets start standing side bends for 1.5 minutes. Starting now!', 95],
         ['Great! Relax now for 10 seconds', 12],
-        ['Lets start standing twists for 2 minutes. Starting now!', 125],
+        ['Lets start standing twists for 1.5 minutes. Starting now!', 95],
         ['Terrific Work! we have completed jumping exercise. See you again!', 0],
         ['You can do additionally Glute Bridges, Modified Plank and Dead Bug Exercise',0],
         
